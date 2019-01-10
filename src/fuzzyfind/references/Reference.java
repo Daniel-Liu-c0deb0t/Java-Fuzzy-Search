8784f191208798f8542daa54915b57f194e1d498
c0deb0t <daniel.liu02@gmail.com>
2019-01-10 13:31:07 -0800

    removed fuzzyfind

--- src/fuzzyfind/references/Reference.java
+++
@@ -1,9 +0,0 @@
-package fuzzyfind.references;
-
-import javafuzzysearch.utils.StrView;
-
-import fuzzyfind.utils.Variables;
-
-public interface Reference{
-    public StrView get(Variables vars);
-}
