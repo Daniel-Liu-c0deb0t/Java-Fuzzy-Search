6b2008592f721413c0210331b87f2e48cccaadbd
c0deb0t <daniel.liu02@gmail.com>
2019-01-21 14:25:17 -0800

    Fixed bugs in scripts

--- scripts/compare_results.py
+++ scripts/compare_results.py
@@ -6,24 +6,35 @@ fastq_file = sys.argv[2]
 correct_idx = set()
 
 with open(correct_file, "r") as f:
-    l = f.readline().strip()
+    while True:
+        l = f.readline().strip()
 
-    if l:
-        correct_idx.add(int(l))
+        if l:
+            correct_idx.add(int(l))
+        else:
+            break
+
+correct = 0
+wrong = 0
 
 with open(fastq_file, "r") as f:
-    l = f.readline().strip()
-    f.readline()
-    f.readline()
-    f.readline()
+    while True:
+        l = f.readline().strip()
+        f.readline()
+        f.readline()
+        f.readline()
 
-    if l:
-        idx = int(l[1:])
+        if l:
+            idx = int(l[1:])
 
-        if idx in correct_idx:
-            correct += 1
+            if idx in correct_idx:
+                correct_idx.remove(idx)
+                correct += 1
+            else:
+                wrong += 1
         else:
-            wrong += 1
+            break
 
 print("Correct reads: " + str(correct))
 print("Wrong reads: " + str(wrong))
+print("Missing reads: " + str(len(correct_idx)))
--- scripts/rand_gbs.py
+++ scripts/rand_gbs.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import random
 from dna_random_gen import rand_edits, rand_str
@@ -24,36 +25,41 @@ with open("barcodes.txt", "w") as f:
     for i in range(num_barcodes):
         barcode_length = random.choice(barcode_lengths)
         barcode = rand_str(barcode_length)
-        barcodes.add(barcode)
+        barcodes.append(barcode)
         f.write("\"barcode_%d\": \"%s\"\n" % (i, barcode))
 
 with_gbs = 0
 
-with open("has_gbs.txt", "w") as f:
-    for i in range(num_iter):
-        print("@" + str(i))
+os.makedirs("gbs_result", exist_ok = True)
+files = [open("gbs_result/has_gbs_barcode_%d.txt" % i, "w") for i in range(len(barcodes))]
 
-        dna_barcode = ""
-        dna_enzyme = ""
-        dna_adapter = ""
+for i in range(num_iter):
+    print("@" + str(i))
 
-        if random.randint(0, 1) == 0:
-            dna_barcode = rand_edits(random.choice(barcodes), edits)
+    dna_barcode = ""
+    dna_enzyme = ""
+    dna_adapter = ""
+    barcode_idx = None
 
-        if random.randint(0, 1) == 0:
-            dna_enzyme = rand_edits(enzyme, edits)
+    if random.randint(0, 1) == 0:
+        barcode_idx = random.randint(0, len(barcodes) - 1)
+        dna_barcode = barcodes[barcode_idx]
+        dna_barcode = rand_edits(dna_barcode, edits)
 
-        if random.randint(0, 1) == 0:
-            dna_adapter = rand_edits(adapter, edits)
+    if random.randint(0, 1) == 0:
+        dna_enzyme = rand_edits(enzyme, edits)
 
-        dna = dna_barcode + dna_enzyme + rand_str(length) + dna_adapter
+    if random.randint(0, 1) == 0:
+        dna_adapter = rand_edits(adapter, edits)
 
-        if dna_barcode and dna_enzyme and dna_adapter:
-            f.write(str(i) + "\n")
-            with_gbs += 1
+    dna = dna_barcode + dna_enzyme + rand_str(length) + dna_adapter
 
-        print(dna)
-        print("+")
-        print("A" * len(dna))
+    if dna_barcode and dna_enzyme and dna_adapter:
+        files[barcode_idx].write(str(i) + "\n")
+        with_gbs += 1
+
+    print(dna)
+    print("+")
+    print("A" * len(dna))
 
 print("Reads with GBS: " + str(with_gbs), file = sys.stderr)
